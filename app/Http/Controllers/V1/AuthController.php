2959590bcc1db7543085a92d8544700f6b193571
chen <569922982@qq.com>
2018-12-15 12:44:31 +0800

    优化

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('refresh', ['except' => ['login', 'register', 'test']]);
+        $this->middleware('jwt.auth', ['except' => ['login', 'register', 'test']]);
     }
 
     public function test()
